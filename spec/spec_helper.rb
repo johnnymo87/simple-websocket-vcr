9955846f74f91af5ffd93bb6c45f24e51af0a75e
Jon Mohrbacher <johnnymo87@gmail.com>
2019-04-28 09:46:50 -0400

    Use lexical scope fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 require 'coveralls'
 Coveralls.wear!
 
+$:.unshift File.expand_path('../../lib/websocket-client-simple/lib', __FILE__)
+$:.unshift File.expand_path('../../lib/websocket-client-simple/lib/event_emitter/lib', __FILE__)
+
 # https://stackoverflow.com/a/9095369/10513533
 # Without this, exceptions coming from code run in threads won't fail tests.
 # For example, WebSocketVCR::RecordableWebsocketClient uses threads.
